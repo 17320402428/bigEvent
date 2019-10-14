ddb321630f54d14719b1dfcc5c0ca21629f7ca5e
haimianbaobao <1940107928@qq.com>
2019-10-14 19:50:23 +0800

    文章管理

---
+++ web_back/tool/category.js
@@ -0,0 +1,32 @@
+var category = {
+    getCate: function (options) {
+        $.ajax({
+            url: CATEGORY_SEARCH,
+            success: options.success
+        });
+    },
+    addCate: function (options) {
+        $.ajax({
+            url: CATEGORY_ADD,
+            type: 'post',
+            data: options.data,
+            success: options.success
+        });
+    },
+    editCate: function (options) {
+        $.ajax({
+            url: CATEGORY_EDIT,
+            type: 'post',
+            data: options.data,
+            success: options.success
+        });
+    },
+    delCate: function (options) {
+        $.ajax({
+            type: 'post',
+            url: CATEGORY_DEL,
+            data : options.data,
+            success : options.success
+        });
+    },
+};
--- web_back/tool/config.js
+++ web_back/tool/config.js
@@ -1,6 +1,11 @@
 var baseUrl = 'http://localhost:8000';
-var USER_LOGIN = baseUrl + '/admin/login',
-    USER_LOGOUT = baseUrl + '/admin/logout',
-    USER_GETINFO = baseUrl + '/admin/getuser',
-    USER_GETXINXI = baseUrl + '/admin/userinfo_get',
-    USER_INFOEDIT = baseUrl + '/admin/userinfo_edit';
+var USER_LOGIN = baseUrl + '/admin/login',//登录接口
+    USER_LOGOUT = baseUrl + '/admin/logout',//登出接口
+    USER_GETINFO = baseUrl + '/admin/getuser',//获取用户信息（两条数据）
+    USER_GETXINXI = baseUrl + '/admin/userinfo_get',//获取用户信息（五条数据）
+    USER_INFOEDIT = baseUrl + '/admin/userinfo_edit',//编辑用户信息
+    USER_SEARCH = baseUrl + '/admin/search',//文章筛选
+    CATEGORY_SEARCH = baseUrl + '/admin/category_search',//文章类别获取
+    CATEGORY_ADD = baseUrl +  '/admin/category_add',//新增文章类别
+    CATEGORY_EDIT = baseUrl +  '/admin/category_edit',//编辑文章类别
+    CATEGORY_DEL = baseUrl + '/admin/category_delete';//删除文章类别
